e8ba2137f86114ce5383ffe805e3f12c8a7fd710
Andre Klang <hej@andreklang.se>
2015-12-21 14:29:31 +0100

    Major refactoring (moved order helpers to AAIT_Shared)
     - The order-helper in the below modules has moved to AAIT_Shared to get rid
       of some code duplication. The affected modules still have their own helper
       but it extends the shared one. The shared one also calls methods in iteslf
       to get correct data, tools & api-helpers.
       Affected modules are:
       - Payexautopay
       - Bankdebit
       - Factoring
       - PartPayment
       - Payex2
       - Wywallet
       Note: Only Payex2 & Bankdebit is currently tested. Our testaccount
       don't have access to Autopay, PartPayment is broken due to falty
       order total, Wywalled doesn't have any test credentials, factoring
       doesn't use the same method (addOrderLine) as the other modules
       (so thats next on my list to fix)

--- Payex/app/code/community/AAIT/Payex2/controllers/Payex2Controller.php
+++ Payex/app/code/community/AAIT/Payex2/controllers/Payex2Controller.php
@@ -63,8 +63,8 @@ class AAIT_Payex2_Payex2Controller extends Mage_Core_Controller_Front_Action
         }
 
         // Get Amount
-        $amount = $order->getGrandTotal();
-        //$amount = Mage::helper('payex2/order')->getCalculatedOrderAmount($order)->amount; // why oh why?
+        //$amount = $order->getGrandTotal();
+        $amount = Mage::helper('payex2/order')->getCalculatedOrderAmount($order)->amount;
 
         // Call PxOrder.Initialize8
         $params = array(
---
+++ Shared/app/code/community/AAIT/Shared/Helper/Api.php
@@ -0,0 +1,9 @@
+<?php
+
+class AAIT_Shared_Helper_Api extends Mage_Core_Helper_Data {
+
+    /**
+     * Currently only a placeholder, but:
+     * todo: get rid of duplicated code and move all modules helper here
+     */
+}
