32617d34b7f960a1afadddccd8fc40afa521ce8d
Andre Klang <hej@andreklang.se>
2015-12-21 10:48:20 +0100

    Fixed data upgrade-script in Autopay
    - An upgrade-script (probably used for migration) throws an error if the
      table "payex_autopay" doesn't exist. And that table is never created..
      Added a try/catch to ignore the error and move on.

--- Autopay/app/code/community/AAIT/Payexautopay/data/payexautopay_setup/data-upgrade-2.0.19-2.0.20.php
+++ Autopay/app/code/community/AAIT/Payexautopay/data/payexautopay_setup/data-upgrade-2.0.19-2.0.20.php
@@ -1,9 +1,15 @@
 <?php
-$records = $this->_conn->fetchAll("SELECT * FROM payex_autopay;");
-foreach ($records as $record) {
-    $agreement = Mage::getModel('payexautopay/agreement');
-    $agreement->setCustomerId($record['customer_id'])
-        ->setAgreementRef($record['agreement_id'])
-        ->setCreatedAt(date('Y-m-d H:i:s', time()))
-        ->save();
+
+try{
+    $records = $this->_conn->fetchAll("SELECT * FROM payex_autopay;");
+    foreach ($records as $record) {
+        $agreement = Mage::getModel('payexautopay/agreement');
+        $agreement->setCustomerId($record['customer_id'])
+            ->setAgreementRef($record['agreement_id'])
+            ->setCreatedAt(date('Y-m-d H:i:s', time()))
+            ->save();
+    }
+} catch (Exception $e) {
+    // We end up here it the table "payex_autopay" doesn't exist
 }
+
