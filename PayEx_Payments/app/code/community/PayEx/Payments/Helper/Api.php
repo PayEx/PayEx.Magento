8d82834ecc4f33a4dfcc282ae5c0ed0dfe7a6b40
aait <info@aait.se>
2016-06-06 14:17:34 +0600

    User Agent header for SOAP requests Close #34

--- PayEx_Payments/app/code/community/PayEx/Payments/Helper/Api.php
+++ PayEx_Payments/app/code/community/PayEx/Payments/Helper/Api.php
@@ -9,13 +9,24 @@ class PayEx_Payments_Helper_Api extends Mage_Core_Helper_Abstract
     /**
      * Get PayEx Api Handler
      * @static
-     * @return Px
+     * @return PayEx\Px
      */
     public static function getPx()
     {
         // Use Singleton
         if (is_null(self::$_px)) {
             self::$_px = new PayEx\Px();
+
+            // Set User Agent
+            $modules = Mage::getConfig()->getNode('modules')->children();
+            $modulesArray = (array)$modules;
+            self::$_px->setUserAgent(sprintf("PayEx.Ecommerce.Php/%s PHP/%s Magento%s/%s PayEx.Magento/%s",
+                \PayEx\Px::VERSION,
+                phpversion(),
+                Mage::getEdition(),
+                Mage::getVersion(),
+                (string)$modulesArray['PayEx_Payments']->version
+            ));
         }
         return self::$_px;
     }
