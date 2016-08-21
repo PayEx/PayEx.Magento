62e112132e3845e23e26a261a09fc1de4049cc50
aait <info@aait.se>
2016-08-21 12:52:39 +0600

    Install script for v3.0.4

---
+++ PayEx_Payments/app/code/community/PayEx/Payments/sql/payex_setup/install-3.0.4.php
@@ -0,0 +1,67 @@
+<?php
+$installer = $this;
+$this->startSetup();
+
+// Install fee
+$this->_conn->addColumn($this->getTable('sales_flat_quote'), 'payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_quote'), 'payex_payment_fee_tax', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_quote'), 'base_payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_quote'), 'base_payex_payment_fee_tax', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee_tax', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee_tax', 'decimal(12,4)');
+
+$eav = new Mage_Sales_Model_Resource_Setup('sales_setup');
+$eav->addAttribute('quote', 'payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('quote', 'payex_payment_fee_tax', array('type' => 'decimal'));
+$eav->addAttribute('quote', 'base_payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('quote', 'base_payex_payment_fee_tax', array('type' => 'decimal'));
+$eav->addAttribute('order', 'payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('order', 'payex_payment_fee_tax', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee_tax', array('type' => 'decimal'));
+
+// Install "fee invoiced" and "fee refunded"
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee_invoiced', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee_tax_invoiced', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee_invoiced', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee_tax_invoiced', 'decimal(12,4)');
+
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee_refunded', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'payex_payment_fee_tax_refunded', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee_refunded', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_order'), 'base_payex_payment_fee_tax_refunded', 'decimal(12,4)');
+
+$this->_conn->addColumn($this->getTable('sales_flat_invoice'), 'payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_invoice'), 'payex_payment_fee_tax', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_invoice'), 'base_payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_invoice'), 'base_payex_payment_fee_tax', 'decimal(12,4)');
+
+$this->_conn->addColumn($this->getTable('sales_flat_creditmemo'), 'payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_creditmemo'), 'payex_payment_fee_tax', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_creditmemo'), 'base_payex_payment_fee', 'decimal(12,4)');
+$this->_conn->addColumn($this->getTable('sales_flat_creditmemo'), 'base_payex_payment_fee_tax', 'decimal(12,4)');
+
+$eav = new Mage_Sales_Model_Resource_Setup('sales_setup');
+$eav->addAttribute('order', 'payex_payment_fee_invoiced', array('type' => 'decimal'));
+$eav->addAttribute('order', 'payex_payment_fee_tax_invoiced', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee_invoiced', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee_tax_invoiced', array('type' => 'decimal'));
+
+$eav->addAttribute('order', 'payex_payment_fee_refunded', array('type' => 'decimal'));
+$eav->addAttribute('order', 'payex_payment_fee_tax_refunded', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee_refunded', array('type' => 'decimal'));
+$eav->addAttribute('order', 'base_payex_payment_fee_tax_refunded', array('type' => 'decimal'));
+
+$eav->addAttribute('invoice', 'payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('invoice', 'payex_payment_fee_tax', array('type' => 'decimal'));
+$eav->addAttribute('invoice', 'base_payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('invoice', 'base_payex_payment_fee_tax', array('type' => 'decimal'));
+
+$eav->addAttribute('creditmemo', 'payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('creditmemo', 'payex_payment_fee_tax', array('type' => 'decimal'));
+$eav->addAttribute('creditmemo', 'base_payex_payment_fee', array('type' => 'decimal'));
+$eav->addAttribute('creditmemo', 'base_payex_payment_fee_tax', array('type' => 'decimal'));
+
+$this->endSetup();
