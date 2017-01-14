3c03f9d03a724db2cf75c567992c85e6727242b9
aait <info@aait.se>
2017-01-14 20:35:49 +0600

    Implemented MobilePay method

---
+++ PayEx_Payments/app/code/community/PayEx/Payments/controllers/MobilepayController.php
@@ -0,0 +1,104 @@
+<?php
+
+class PayEx_Payments_MobilepayController extends Mage_Core_Controller_Front_Action
+{
+    public function _construct()
+    {
+        // Bootstrap PayEx Environment
+        Mage::getSingleton('payex/payment_mobilepay');
+    }
+
+    /**
+     * @throws Exception
+     * @throws Mage_Core_Exception
+     */
+    public function redirectAction()
+    {
+        Mage::helper('payex/tools')->addToDebug('Controller: redirect');
+
+        // Load Order
+        $order_id = Mage::getSingleton('checkout/session')->getLastRealOrderId();
+
+        /** @var Mage_Sales_Model_Order $order */
+        $order = Mage::getModel('sales/order');
+        $order->loadByIncrementId($order_id);
+        if (!$order->getId()) {
+            Mage::throwException('No order for processing found');
+        }
+
+        // Set quote to inactive
+        Mage::getSingleton('checkout/session')->setPayexQuoteId(Mage::getSingleton('checkout/session')->getQuoteId());
+        Mage::getSingleton('checkout/session')->getQuote()->setIsActive(false)->save();
+        Mage::getSingleton('checkout/session')->clear();
+
+        /** @var PayEx_Payments_Model_Payment_Abstract $method */
+        $method = $order->getPayment()->getMethodInstance();
+
+        // Get Currency code
+        $currency_code = $order->getOrderCurrency()->getCurrencyCode();
+
+        // Get Operation Type (AUTHORIZATION / SALE)
+        $operation = ($method->getConfigData('transactiontype') == 0) ? 'AUTHORIZATION' : 'SALE';
+
+        // Get Amount
+        //$amount = $order->getGrandTotal();
+        $amount = Mage::helper('payex/order')->getCalculatedOrderAmount($order)->getAmount();
+
+        // Call PxOrder.Initialize8
+        $params = array(
+            'accountNumber' => '',
+            'purchaseOperation' => $operation,
+            'price' => round($amount * 100),
+            'priceArgList' => '',
+            'currency' => $currency_code,
+            'vat' => 0,
+            'orderID' => $order_id,
+            'productNumber' => $order_id,
+            'description' => Mage::app()->getStore()->getName(),
+            'clientIPAddress' => Mage::helper('core/http')->getRemoteAddr(),
+            'clientIdentifier' => 'USERAGENT=' . Mage::helper('core/http')->getHttpUserAgent(),
+            'additionalValues' => 'RESPONSIVE=1&USEMOBILEPAY=True',
+            'externalID' => '',
+            'returnUrl' => Mage::getUrl('payex/payment/success', array('_secure' => true)),
+            'view' => 'CREDITCARD',
+            'agreementRef' => '',
+            'cancelUrl' => Mage::getUrl('payex/payment/cancel', array('_secure' => true)),
+            'clientLanguage' => $method->getConfigData('clientlanguage')
+        );
+        $result = Mage::helper('payex/api')->getPx()->Initialize8($params);
+        Mage::helper('payex/tools')->addToDebug('PxOrder.Initialize8:' . $result['description']);
+
+        // Check Errors
+        if ($result['code'] !== 'OK' || $result['description'] !== 'OK' || $result['errorCode'] !== 'OK') {
+            $message = Mage::helper('payex/tools')->getVerboseErrorMessage($result);
+
+            // Cancel order
+            $order->cancel();
+            $order->addStatusHistoryComment($message, Mage_Sales_Model_Order::STATE_CANCELED);
+            $order->save();
+
+            // Set quote to active
+            if ($quoteId = Mage::getSingleton('checkout/session')->getPayexQuoteId()) {
+                $quote = Mage::getModel('sales/quote')->load($quoteId);
+                if ($quote->getId()) {
+                    $quote->setIsActive(true)->save();
+                    Mage::getSingleton('checkout/session')->setQuoteId($quoteId);
+                }
+            }
+
+            Mage::getSingleton('checkout/session')->addError($message);
+            $this->_redirect('checkout/cart');
+            return;
+        }
+        $order_ref = $result['orderRef'];
+        $redirectUrl = $result['redirectUrl'];
+
+        // Set Pending Payment status
+        $order->setState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT, Mage_Sales_Model_Order::STATE_PENDING_PAYMENT, Mage::helper('payex')->__('The customer was redirected to PayEx.'));
+        $order->save();
+
+        // Redirect to PayEx
+        header('Location: ' . $redirectUrl);
+        exit();
+    }
+}
